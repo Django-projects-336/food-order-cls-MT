90b3a57639b4794f9efc96df1a5e4517859ab281
RatnaTeja9 <ratnateja.ds@gmail.com>
2026-05-06 14:36:56 +0530

    added cart , manage menu views

--- frontend/urls.py
+++ frontend/urls.py
@@ -3,4 +3,6 @@ from frontend import views
 
 urlpatterns = [
     path("", views.homepage, name="homepage"),
+    path("cart/", views.cart, name="cart"),
+    path("manage-menu/", views.manage_menu, name="manage-menu"),
 ]
